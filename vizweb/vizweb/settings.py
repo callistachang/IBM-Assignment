15b8fac5f92a4f94ee89aa6a7f6ddfcfcb9fb324
callistachang <changcallista@gmail.com>
2020-11-15 16:47:34 +0800

    Set up dotenv for secret keys

--- vizweb/vizweb/settings.py
+++ vizweb/vizweb/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
+load_dotenv()
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -18,6 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
+
+SECRET_KEY = os.getenv("SECRET_KEY", default="dev_key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
