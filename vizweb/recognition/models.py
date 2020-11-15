7b57377fa84c5f94e10018809386081bbede11f0
callistachang <changcallista@gmail.com>
2020-11-16 02:25:14 +0800

    Update ImageLabel model

--- vizweb/recognition/models.py
+++ vizweb/recognition/models.py
@@ -11,3 +11,6 @@ class ImageLabel(models.Model):
     image = models.ForeignKey(Image, on_delete=models.CASCADE, related_name="labels")
     label = models.CharField(max_length=50)
     score = models.FloatField()
+
+    class Meta:
+        ordering = ["-score"]
