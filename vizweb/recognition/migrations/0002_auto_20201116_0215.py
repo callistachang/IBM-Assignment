09d1d8fdb356c89dbc56d14dc341a64e54f9eeb0
callistachang <changcallista@gmail.com>
2020-11-16 03:40:58 +0800

    Final run of black code formatting

--- vizweb/recognition/migrations/0002_auto_20201116_0215.py
+++ vizweb/recognition/migrations/0002_auto_20201116_0215.py
@@ -11,7 +11,6 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.AlterModelOptions(
-            name="imagelabel",
-            options={"ordering": ["-score"]},
+            name="imagelabel", options={"ordering": ["-score"]},
         ),
     ]
